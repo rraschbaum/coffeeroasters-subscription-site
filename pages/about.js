c4615b71c1eac177d7e99836df849f739b39f51d
Ryan Raschbaum <ryanraschbaum@Ryans-MacBook-Pro.local>
2022-06-24 13:57:40 -0400

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,46 @@
+function About() {
+  return (
+    <div>
+      <div>Home About us Create your plan</div>
+      <div>About us </div>
+      <div>
+        Coffeeroasters began its journey of exotic discovery in 1999,
+        highlighting stories of coffee from around the world. We have since been
+        dedicated to bring the perfect cup - from bean to brew - in every
+        shipment.
+      </div>
+      <div>Our commitment</div>
+      <div>
+        We’re built on a simple mission and a commitment to doing good along the
+        way. We want to make it easy for you to discover and brew the world’s
+        best coffee at home. It all starts at the source. To locate the specific
+        lots we want to purchase, we travel nearly 60 days a year trying to
+        understand the challenges and opportunities in each of these places. We
+        collaborate with exceptional coffee growers and empower a global
+        community of farmers through with well above fair-trade benchmarks. We
+        also offer training, support farm community initiatives, and invest in
+        coffee plant science. Curating only the finest blends, we roast each lot
+        to highlight tasting profiles distinctive to their native growing
+        region.
+      </div>
+      <div>Uncompromising quality</div>
+      <div>
+        Although we work with growers who pay close attention to all stages of
+        harvest and processing, we employ, on our end, a rigorous quality
+        control program to avoid over-roasting or baking the coffee dry. Every
+        bag of coffee is tagged with a roast date and batch number. Our goal is
+        to roast consistent, user-friendly coffee, so that brewing is easy and
+        enjoyable.
+      </div>
+      <div>Our headquarters</div>
+      <div>United Kingdom 68 Asfordby Rd Alcaston SY6 1YA +44 1241 918425</div>
+      <div>
+        Canada 1528 Eglinton Avenue Toronto Ontario M4P 1A6 +1 416 485 2997
+      </div>
+      <div>Australia 36 Swanston Street Kewell Victoria +61 4 9928 3629</div>
+      <div>Home About us Create your plan</div>
+    </div>
+  );
+}
+
+export default About;
---
+++ pages/plan.js
@@ -0,0 +1,38 @@
+function Plan() {
+  return (
+    <div>
+      <div>Home About us Create your plan</div>
+      <div>Create a plan</div>
+      <div>
+        Build a subscription plan that best fits your needs. We offer an
+        assortment of the best artisan coffees from around the globe delivered
+        fresh to your door.
+      </div>
+      <div>01</div>
+      <div>Pick your coffee</div>
+      <div>
+        Select from our evolving range of artisan coffees. Our beans are
+        {/* ethically sourced and we pay fair prices for them. There's new coffees */}
+        in all profiles every month for you to try out.
+      </div>
+      <div>02</div>
+      <div>Choose the frequency</div>
+      <div>
+        Customize your order frequency, quantity, even your roast style and
+        grind type. Pause, skip or cancel your subscription with no commitment
+        through our online portal.
+      </div>
+      <div>03</div>
+      <div>Receive and enjoy!</div>
+      <div>
+        We ship your package within 48 hours, freshly roasted. Sit back and
+        enjoy award-winning world-class coffees curated to provide a distinct
+        tasting experience.
+      </div>
+
+      <div>Home About us Create your plan</div>
+    </div>
+  );
+}
+
+export default Plan;
